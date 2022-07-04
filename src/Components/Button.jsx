cb4a682f3e590dfb0182eec8616efb1866c68b5f
RaezaMLN <raezam85@gmail.com>
2022-07-04 12:14:25 +0700

    Inisiate Button Component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+function Button({ title, btnClass, to }) {
+  return (
+    <div>
+      <button type="submit" className={btnClass}>
+        {title}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
