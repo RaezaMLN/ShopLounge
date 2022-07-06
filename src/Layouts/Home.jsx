ad7124f314c2a8f5e19a6520e8ef96d0cfb95f9e
RaezaMLN <raezam85@gmail.com>
2022-07-06 07:08:26 +0700

    create Banner on Home Layout

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "../Components/Button";
 import sofa from "../img/sofa.png";
 import lamp from "../img/lamp.png";
 
@@ -15,14 +16,15 @@ export default function Home() {
             <p className="fs-5 clr2 lato fw-bold">Best Furniture For Your Castle....</p>
             <p className="fs-1 josefin fw-bold">New Furniture Collection Trends in 2020</p>
             <p className="fs-5 lato">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.</p>
-            <Button>
-              <p className="fs-4 josefin">Shop Now</p>
-            </Button>
+            <Button btnClass={"btn text-light"} btnTitle={"Shop Now"} btnStyle={{ backgroundColor: "#fb2e86" }} />
           </Col>
           <Col xs={5}>
             <img src={sofa} alt="" className="w-75 py-3" />
           </Col>
         </Row>
+      </Container>
+      <Container fluid className="clr3">
+        <Row></Row>
       </Container>
     </div>
   );
