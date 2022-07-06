914aa81e61735cdb8ef2c2385c88ba1bf8790f92
RaezaMLN <raezam85@gmail.com>
2022-07-06 08:02:48 +0700

    create Feature with card on it, in Home Page

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Carousel } from "react-bootstrap";
 import Button from "../Components/Button";
 import sofa from "../img/sofa.png";
 import lamp from "../img/lamp.png";
@@ -7,6 +7,7 @@ import lamp from "../img/lamp.png";
 export default function Home() {
   return (
     <div>
+      {/* Banner */}
       <Container fluid>
         <Row className="clr1">
           <Col xs={3} className="d-flex flex-column align-items-end">
@@ -15,16 +16,51 @@ export default function Home() {
           <Col xs={4} className="d-flex flex-column align-items-start justify-content-center">
             <p className="fs-5 clr2 lato fw-bold">Best Furniture For Your Castle....</p>
             <p className="fs-1 josefin fw-bold">New Furniture Collection Trends in 2020</p>
-            <p className="fs-5 lato">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.</p>
-            <Button btnClass={"btn text-light"} btnTitle={"Shop Now"} btnStyle={{ backgroundColor: "#fb2e86" }} />
+            <p className="fs-5 lato" style={{ color: "#8A8FB9" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.
+            </p>
+            <Button btnClass={"btn text-light josefin"} btnTitle={"Shop Now"} btnStyle={{ backgroundColor: "#fb2e86" }} />
           </Col>
           <Col xs={5}>
             <img src={sofa} alt="" className="w-75 py-3" />
           </Col>
         </Row>
       </Container>
+
+      {/* Feature  */}
       <Container fluid className="clr3">
         <Row></Row>
+      </Container>
+      <Container>
+        <Row className="my-5">
+          <Col>
+            <h1 className="fs-2 fw-bold clr4 text-center josefin">Featured Products</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }} className="border border-0">
+              {/* Carousel  */}
+              <Carousel>
+                <Carousel.Item>
+                  <img className="d-block w-100 clr0" src={sofa} alt="First slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100 clr0" src={sofa} alt="Second slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100 clr0" src={sofa} alt="Third slide" />
+                </Carousel.Item>
+              </Carousel>
+              <Card.Body className="text-center">
+                <Card.Title className="clr2 josefin fw-bold">Cantilever chair</Card.Title>
+                <Card.Text className="clr4 lato fw-semibold">Code - Y523201</Card.Text>
+                <Card.Text className="clr4 lato fw-semibold">$42.00</Card.Text>
+              </Card.Body>
+              <Card.Body></Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
