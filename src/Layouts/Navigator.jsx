54893c25952e144962e348fbc94d62cc2f30faea
RaezaMLN <raezam85@gmail.com>
2022-07-07 19:37:50 +0700

    create OrderCompleted page to Layouts and Set the Navigator too

--- src/Layouts/Navigator.jsx
+++ src/Layouts/Navigator.jsx
@@ -7,7 +7,8 @@ import Home from "./Home";
 import ShoppingCart from "./ShoppingCart";
 import ShopeGrid from "./ShopGrid";
 import Header from "../Components/Header";
-import Footer from "../Components/Footer"
+import Footer from "../Components/Footer";
+import OrderCompleted from "./OrderCompleted";
 
 export default function Navigator() {
   return (
@@ -18,8 +19,9 @@ export default function Navigator() {
           <Route path="/" exact element={<Home />} />
           <Route path="shopping-cart" exact element={<ShoppingCart />} />
           <Route path="shope-grid" exact element={<ShopeGrid />} />
+          <Route path="order-completed" exact element={<OrderCompleted />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/Layouts/OrderCompleted.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import GreyContainer from "../Components/GreyContainer";
+export default function OrderCompleted() {
+  return (
+    <Container>
+      <GreyContainer titlePage={"Order Completed"} />
+    </Container>
+  );
+}
