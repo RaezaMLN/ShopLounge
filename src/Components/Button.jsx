a9c12cced217c1c823f029bab571b26dd5fc31fc
Ferdy Aqliyansyah <ferdyaqli27@gmail.com>
2022-07-13 15:19:35 +0700

    fix recursive func in button component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "../../src/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { Button } from "react-bootstrap";
-function Button({ btnVariant, btnStyle, btnClass, btnTitle }) {
+import { Button } from "react-bootstrap";
+function CButton({ btnVariant, btnStyle, btnClass, btnTitle }) {
   return (
     <div>
       <Button variant={btnVariant} style={btnStyle} className={btnClass}>
@@ -12,4 +12,4 @@ function Button({ btnVariant, btnStyle, btnClass, btnTitle }) {
   );
 }
 
-export default Button;
+export default CButton;
