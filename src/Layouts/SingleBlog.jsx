12ec7958c2ff3fd84d59211d520583673798d43b
RaezaMLN <raezam85@gmail.com>
2022-07-25 19:10:32 +0700

    add validation and alert popUp on SingleBlog Form

--- src/Layouts/SingleBlog.jsx
+++ src/Layouts/SingleBlog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import GreyContainer from "../Components/GreyContainer";
 import Button from "../Components/Button";
@@ -30,11 +30,31 @@ import offer2 from "../img/offer2.png";
 import offer3 from "../img/offer3.png";
 import offer4 from "../img/offer4.png";
 
+import Swal from "sweetalert2";
 import { FiSearch } from "react-icons/fi";
 
 export default function SingleBlog() {
   const navigate = useNavigate();
+  const [validated, setValidated] = useState(false);
 
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      Swal.fire({
+        title: "Your Comment Has Been Send",
+        showClass: {
+          popup: "animate__animated animate__fadeInDown",
+        },
+        hideClass: {
+          popup: "animate__animated animate__fadeOutUp",
+        },
+      });
+    }
+    setValidated(true);
+  };
   return (
     <Container fluid>
       <GreyContainer titlePage={"Single Blog"} />
@@ -194,25 +214,18 @@ export default function SingleBlog() {
           <br />
           <br />
 
-          <Form className="w-75">
+          <Form className="w-75" onSubmit={handleSubmit} noValidate validated={validated}>
             <Form.Group className="mb-5 d-flex" controlId="formBasicTextt">
-              <Form.Control type="text" placeholder="Your name*" className="color-wildBlueYonder lato me-3" />
-              <Form.Control type="text" placeholder="Write your email*" className="color-wildBlueYonder lato" />
+              <Form.Control type="text" placeholder="Your name*" className="color-wildBlueYonder lato me-3" required />
+              <Form.Control type="text" placeholder="Write your email*" className="color-wildBlueYonder lato" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-              <Form.Control as="textarea" rows={3} placeholder="Write your comment*" className="py-5" />
+              <Form.Control as="textarea" rows={3} placeholder="Write your comment*" className="py-5" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Save my name, email, and website in this browser for the next time I comment." />
+              <Form.Check type="checkbox" label="Save my name, email, and website in this browser for the next time I comment." required />
             </Form.Group>
-            <Button
-              btnClass={"btn text-light josefin my-5 w-100"}
-              btnTitle={"Continue Shopping"}
-              btnStyle={{ backgroundColor: "#fb2e86" }}
-              eventClick={() => {
-                navigate("/shop-grid");
-              }}
-            />
+            <Button btnType={"submit"} btnClass={"btn text-light josefin my-5 w-100"} btnTitle={"Continue Shopping"} btnStyle={{ backgroundColor: "#fb2e86" }} />
           </Form>
         </div>
 
